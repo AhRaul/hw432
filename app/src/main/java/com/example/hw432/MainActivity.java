dd68b376b18b911ec1a6708e2ac12c09bb9796fd
gg <gartanolio@yandex.ru>
2019-09-15 22:19:43 +0300

    hw432 task2, ToolbarActivity with progressBar, and buttons for show and hide this progressBar

--- app/src/main/java/com/example/hw432/MainActivity.java
+++ app/src/main/java/com/example/hw432/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hw432;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,10 +81,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
+        if (id == R.id.go_task_two) {
+            Intent intent = new Intent(MainActivity.this, ToolbarActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
